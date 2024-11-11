45c3a29c8f7c0b2bb12fb63e12f5b0af860f0503
amadousadialloh <amadousadialloh@gmail.com>
2024-11-11 10:14:56 -0600

    section 2 completed

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 
 const port = 3000;
 
-app.get("/greetings/:username", (req, res) => {
-  res.send(`hello there ${req.params.username}`);
+// app.get("/greetings/:username", (req, res) => {
+//   res.send(`hello there ${req.params.username}`);
+// });
+
+app.get("/roll/:number", (req, res) => {
+  let rolledNumber = req.params.number;
+  if (isNaN(rolledNumber)) {
+    res.send(`<p>You must specify a number</p>`);
+  } else {
+    const randomNumber = Math.floor(Math.random() * rolledNumber);
+    res.send(`<p>You rolled a ${randomNumber}</p>`);
+  }
 });
 
 app.listen(port, () => {
